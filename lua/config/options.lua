4cc70f60b1c59607b040ad566df06b90a9b67952
zhichao <sunzhichao@ihaoin.com>
2026-06-12 18:47:22 +0800

    feat: iTerm2 标签页显示项目名/当前文件夹

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,7 @@
 vim.opt.scrolloff = 8
 vim.g.lazyvim_eslint_auto_format = false
 vim.g.lazyvim_ts_lsp = "vtsls"
+
+-- iTerm2 标签页显示：项目名/当前文件夹
+vim.opt.title = true
+vim.opt.titlestring = "%{fnamemodify(getcwd(), ':t')}/%{expand('%:h:t')}"
